bf4cd8a99a84a5e0b985c46908dbc27372ac4c6d
Damian Juzyk <damian.juzyk@gmail.com>
2018-02-03 12:00:29 +0400

    add poprawa webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -19,10 +19,11 @@ if (env === 'production') {
 }
 
 module.exports = {
-    entry: [
+    entry: (env !== 'production' ? [
         'react-hot-loader/patch',
-        './client/index.js'
-    ],
+        'webpack-dev-server/client?http://localhost:8080',
+        'webpack/hot/only-dev-server',
+    ] : []).concat(['./client/index.js']),
     output: {
         path: path.resolve(__dirname, 'build'),
         filename: './bundle.js'
